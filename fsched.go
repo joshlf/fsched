f279b0433cc5d298298c6ebe5593254254fdb266
Joshua Liebow-Feeser <hello@joshlf.com>
2013-10-25 01:13:44 -0400

    Added LICENSE, AUTHORS files; added copyright comments to source files.

--- fsched.go
+++ fsched.go
@@ -1,3 +1,7 @@
+// Copyright 2013 The Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // Note that this doc comment shares text with the Scheduler documentation.
 // Please keep in sync.
 
